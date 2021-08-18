cdd2a05011e984ed8b099afad4d9f5e8a53bde65
Artem <artemklinkov@yandex.ru>
2021-08-18 21:09:36 +0300

    004-rest-ex2

---
+++ app/Http/Controllers/ApiTokenController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\ApiTokenRequest;
+use Illuminate\Support\Facades\Hash;
+
+class ApiTokenController extends Controller
+{
+    public function createToken(ApiTokenRequest $request)
+    {
+        $user = User::where('email', $request->email)->first();
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json(['error' => 'The provided credentials are incorrect'], 401);
+        }
+        $token = $user->createToken($request->device_name);
+        return ['token' => $token->plainTextToken];
+    }
+}
